e093279cd3a301710b198d7bcf6f26b50c98c715
Dang Thien Doan <truong.thien.doan@gmail.com>
2015-08-21 15:10:12 -0400

    Changing run script to an init.d script

--- lichebot-dist.sh
+++ lichebot-dist.sh
@@ -8,6 +8,29 @@ export HUBOT_HIPCHAT_PASSWORD=""
 #export HUBOT_HIPCHAT_XMPP_DOMAIN="btf.hipchat.com"
 #export HUBOT_HIPCHAT_RECONNECT="true"
 
+# Fill this value to the LicheBot root directory
+HOME_DIRECTORY='';
 
+cd $HOME_DIRECTORY;
 
-bin/hubot --adapter hipchat
+case "$1" in
+  start)
+    ./bin/hubot --adapter hipchat &
+    echo $! >> /tmp/lichebot.pid
+    echo 'Process started.';
+    ;;
+  stop)
+    kill -9 $(cat /tmp/lichebot.pid) 2> /dev/null || echo "Process is not running."
+    echo 'Process stopped.';
+    ;;
+  restart)
+    kill -9 $(cat /tmp/lichebot.pid) 2> /dev/null || echo "Process is not running."
+    ./bin/hubot --adapter hipchat &
+    echo  $! >> /tmp/lichebot.pid
+    echo 'Process started.';
+    ;;
+  *)
+    echo "Usage: ($0){start|stop|restart}" >&2
+    exit 3
+    ;;
+esac
