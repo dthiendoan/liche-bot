879ff618aec0dad5d8f09f6a75db02dc43cf8a8f
Dang Truong Thien Doan <truong.thien.doan@gmail.com>
2015-11-05 11:55:46 -0800

    Setting up additional rules

--- scripts/Roulette.js
+++ scripts/Roulette.js
@@ -1,13 +1,21 @@
 var Roulette = function (robot) {
   var shot;
+  var lastPerson;
 
   var spinBarrel = function () {
     shot = Math.floor(Math.random() * 6);
   }
 
-  var pullTrigger = function () {
+  var pullTrigger = function (person) {
+    if (person == lastPerson) {
+      return;
+    }
+
+    lastPerson = person;
+
     if (shot == 0) {
       spinBarrel();
+      lastPerson = null;
       return true;
     } else {
       shot--;
@@ -18,10 +26,13 @@ var Roulette = function (robot) {
   spinBarrel();
 
   robot.hear(/^\/roulette\s*$/, function (msg) {
-    if (pullTrigger()) {
-      msg.reply('*BANG* YOU LOSE !');
-    } else {
-      msg.reply('*click*');
+    var result = pullTrigger(msg.message.user.name);
+    if (result === true) {
+      msg.emote(' - *BANG* YOU LOSE !');
+    } else if (result === undefined) {
+      msg.reply('are you suicidal ?');
+    } else if (result === false) {
+      msg.emote(' - *click*');
     }
   });
 };
