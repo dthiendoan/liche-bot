bf88cbc12e46263ecd929a1bdb02d6d7dc71e824
Dang Truong Thien Doan <truong.thien.doan@gmail.com>
2015-11-05 11:55:46 -0800

    Setting up additional rules

--- scripts/Roulette.js
+++ scripts/Roulette.js
@@ -1,13 +1,20 @@
 var Roulette = function (robot) {
   var shot;
+  var lastPerson;
 
   var spinBarrel = function () {
     shot = Math.floor(Math.random() * 6);
   }
 
-  var pullTrigger = function () {
+  var pullTrigger = function (person) {
+    if (person == lastPerson) {
+
+      return;
+    }
+
     if (shot == 0) {
       spinBarrel();
+      lastPerson = null;
       return true;
     } else {
       shot--;
