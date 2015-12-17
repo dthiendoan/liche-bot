99cc55c712aab7540f83b89a738d29c3b1d17fa9
Dang Thien Doan <truong.thien.doan@gmail.com>
2015-12-17 17:16:37 -0500

    Updates infos

--- scripts/Info.js
+++ scripts/Info.js
@@ -5,7 +5,9 @@ var Info = function(robot) {
       '/gygax: Rolls character attributes with 3d6\n' +
       '/gygax_4d6: Rolls character attributes with 4d6 and removes the lowest die\n' +
       '/food [list|remove {name}|add {name}|pick]\n' +
-      '/gossip [add {gossip_text}|remove {id}|{word_filters}]\n'
+      '/gossip [add {gossip_text}|remove {id}|{word_filters}]\n' +
+      '/roulette [|spin|stats]\n' +
+      '/deck [pick|deal|shuffle]'
     );
 
   });
