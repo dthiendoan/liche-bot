fe177850690081bfdb41b9aa03b93035ae10b67b
David Zou <david.zou@outlook.com>
2017-07-25 16:40:12 -0700

    [wip] need to instantiate classes...

--- scripts/HighNoon.js
+++ scripts/HighNoon.js
@@ -7,6 +7,7 @@ var UserInterface = require('./lib/highnoon/UserInterface.js');
 var SessionStore = require('./lib/highnoon/SessionStore.js');
 var GameEngine = require('./lib/highnoon/GameEngine.js');
 
+
 help.setHelpCategory(
   'highnoon',
   'mcree\'s favorite time of the day',
@@ -18,47 +19,49 @@ help.setHelpCategory(
 
 function Highnoon (robot) {
   var trigger = new TriggerHelper('highnoon');
+  var UI = new UserInterface();
+  var GE = new GameEngine();
 
   robot.hear(trigger.getTrigger(), function (msg) {
     var person = messageHelper.getPerson(msg);
     var channelId = messageHelper.getChannelId(msg);
-    var result = UserInterface.createSession(SessionStore[channelId], channelId, person);
-    UserInterface.checkSession(msg, result, channelId, person);
+    var result = UI.createSession(SessionStore[channelId], channelId, person);
+    UI.checkSession(msg, result, channelId, person);
   });
 
   robot.hear(trigger.getTrigger('(\\d+?)'), function (msg) {
     var person = messageHelper.getPerson(msg);
     var channelId = messageHelper.getChannelId(msg);
-    var result = UserInterface.createSession(SessionStore[channelId], channelId, person, msg.match[1]);
-    UserInterface.checkSession(msg, result, channelId, person);
+    var result = UI.createSession(SessionStore[channelId], channelId, person, msg.match[1]);
+    UI.checkSession(msg, result, channelId, person);
   });
 
   robot.hear(trigger.getTrigger('removePlayer', '([\\s\\S]+?)'), function (msg) {
     var person = msg.match[1];
     var channelId = messageHelper.getChannelId(msg);
-    var result = UserInterface.removePlayer(channelId, person);
-    UserInterface.checkSession(msg, result, channelId, person);
+    var result = UI.removePlayer(channelId, person);
+    UI.checkSession(msg, result, channelId, person);
   });
 
   robot.hear(trigger.getTrigger('test'), function (msg) {
     var channelId = messageHelper.getChannelId(msg);
-    GameEngine.testDuel(msg, SessionStore[channelId]);
+    GE.testDuel(msg, SessionStore[channelId]);
   });
 
   robot.hear(/BANG!/i, function (msg) {
     var shooter = messageHelper.getPerson(msg);
     var channelId = messageHelper.getChannelId(msg);
-    GameEngine.checkShot(msg, SessionStore[channelId], channelId, shooter);
+    GE.checkShot(msg, SessionStore[channelId], channelId, shooter);
   });
 
   robot.hear(trigger.getTrigger('cancel'), function (msg) {
     var channelId = messageHelper.getChannelId(msg);
-    UserInterface.removeSession(msg, channelId);
+    UI.removeSession(msg, channelId);
   });
 
   robot.hear(trigger.getTrigger('time'), function (msg) {
     var channelId = messageHelper.getChannelId(msg);
-    GameEngine.checkTime(msg, SessionStore[channelId], channelId);
+    GE.checkTime(msg, SessionStore[channelId], channelId);
   });
 };
 
