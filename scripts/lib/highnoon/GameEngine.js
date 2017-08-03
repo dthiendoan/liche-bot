fae37f40f8796178cf12a6bd6ed0b58dba7ab2bf
David Zou <david.zou@outlook.com>
2017-08-03 16:22:07 -0700

    [cleanup] moved causes of deaths to new file

--- scripts/lib/highnoon/GameEngine.js
+++ scripts/lib/highnoon/GameEngine.js
@@ -19,28 +19,7 @@ var PLAYER_DID_NOT_JOIN = require('./lib/States.js').PLAYER_DID_NOT_JOIN;
 var FAILED_TO_RECORD_RESULTS = require('./lib/States.js').FAILED_TO_RECORD_RESULTS;
 var RESULTS_SUCCESSFULLY_RECORDED = require('./lib/States.js').RESULTS_SUCCESSFULLY_RECORDED;
 
-var CAUSE_OF_DEATH = [
-  'right at the heart! Gae bolg style, bitches.',
-  'right at the neck! Blood starts squirting everywhere.',
-  'right at the knee! No more adventures for this lad...',
-  'right at the family jewels! Ouch...Ramsay Bolton would be proud.',
-  'square at the head! OOOOOOH, H-H-H-HEADSHOT! YOU CAN DANCE ALL DAY!',
-  'by shooting the bullet at a metal wall... which ricocheted to a metal street sign... which finally ricocheted to the head.',
-  'by throwing the gun at the floor, causing the trigger to go off and miraculously hit.',
-  'with a crossbow. What???  I thought this was a gun duel... Anyway.',
-  'with a spear at the chest. THIS IS MADNESS!',
-  'with LOVE ARROW SHOOT!!!',
-  'with a smartphone camera.  Then proceeds to shoot with a regular pistol.',
-  'with a railgun, creating a large hole in the body.',
-  'with a nerf gun...full of gasoline.  Then throws a lit match.',
-  'while doing a somersault! WATCH THIS!',
-  'with a flamethrower, BURN BABY BURN!!!',
-  'with a nail gun!  You can say that was...crafty. *Sunglasses*',
-  'with the holy words from \'Fifty Shades of Grey\'!  Ears start bleeding!',
-  'with a paper plane laced with ricin poison.',
-  'right at the hand with the gun, causing that gun to misfire and explode!',
-  'with dysentery!'
-];
+var CAUSE_OF_DEATH = require('./lib/Causes.js').CAUSE_OF_DEATH;
 
 var COUNTDOWN_INTERVAL = 1500; 
 
---
+++ scripts/lib/highnoon/lib/Causes.js
@@ -0,0 +1,22 @@
+exports.CAUSE_OF_DEATH = [
+  'right at the heart! Gae bolg style, bitches.',
+  'right at the neck! Blood starts squirting everywhere.',
+  'right at the knee! No more adventures for this lad...',
+  'right at the family jewels! Ouch...Ramsay Bolton would be proud.',
+  'square at the head! OOOOOOH, H-H-H-HEADSHOT! YOU CAN DANCE ALL DAY!',
+  'by shooting the bullet at a metal wall... which ricocheted to a metal street sign... which finally ricocheted to the head.',
+  'by throwing the gun at the floor, causing the trigger to go off and miraculously hit.',
+  'with a crossbow. What???  I thought this was a gun duel... Anyway.',
+  'with a spear at the chest. THIS IS MADNESS!',
+  'with LOVE ARROW SHOOT!!!',
+  'with a smartphone camera.  Then proceeds to shoot with a regular pistol.',
+  'with a railgun, creating a large hole in the body.',
+  'with a nerf gun...full of gasoline.  Then throws a lit match.',
+  'while doing a somersault! WATCH THIS!',
+  'with a flamethrower, BURN BABY BURN!!!',
+  'with a nail gun!  You can say that was...crafty. *Sunglasses*',
+  'with the holy words from \'Fifty Shades of Grey\'!  Ears start bleeding!',
+  'with a paper plane laced with ricin poison.',
+  'right at the hand with the gun, causing that gun to misfire and explode!',
+  'with dysentery!'
+];
